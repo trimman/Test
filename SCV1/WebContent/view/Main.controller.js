b6a2a832aa7c1dc6b561f8026b80ded0daeac09d
trimman <sapdeveloperabap@gmail.com>
2015-07-30 14:19:32 +0200

    initial commit

---
+++ SCV1/WebContent/view/Main.controller.js
@@ -0,0 +1,8 @@
+sap.ui.controller("SCV_test1.view.Main", {
+
+    onInit : function() {
+        if (sap.ui.Device.support.touch === false) {
+            this.getView().addStyleClass("sapUiSizeCompact");
+        }
+    }
+});
